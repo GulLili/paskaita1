4f0907e2885efdbbf8b3011ad38344942ef797ef
Lilija G <lilija.gulbickaja@gmail.com>
2020-03-04 10:50:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace paskaita1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sveiki, koks jūsų vardas?");
+            string vartotojoVardas = Console.ReadLine();
+            Console.WriteLine("Jus įvedėte" + " " + vartotojoVardas);
+            
+        }
+    }
+}
